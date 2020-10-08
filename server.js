09c1e2f42f22dc2994f40657aef0ffb5f4710d0b
William Rocha <wrrocha.verx@rd.com.br>
2020-10-07 23:43:19 -0300

    feat: redeploy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var serveStatic = require('serve-static')
 var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
+console.log("pre start server.js")
 var port = process.env.PORT || 8080
 app.listen(port)
 console.log('server started ' + port)
